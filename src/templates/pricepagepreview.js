4033fe73e5298d8b95834b2b4a854e5f7f7d6fa2
MahasiswaLuthfiyah <luthfiareta123@gmail.com>
2025-04-27 22:54:04 +0700

    fix: add missing PricePagePreview.js

---
+++ src/templates/pricepagepreview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PricePagePreview = ({ entry }) => {
+  return (
+    <div>
+      <h1>Preview Harga</h1>
+      {/* Kamu bisa tambahkan preview data dari entry jika mau */}
+    </div>
+  );
+};
+
+export default PricePagePreview;
